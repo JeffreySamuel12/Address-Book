531ca1e612f74288fc5c105b116b0f5e471dbc6f
Jeffrey Samuel <Jeffrey.Samuel@godigit.com>
2024-07-26 17:14:13 +0530

    Added functionality to add multiple contacts to Address Book

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -7,18 +7,33 @@ public class AddressBookMain {
         AddressBook addressBook = new AddressBook();
         Scanner scanner = new Scanner(System.in);
 
-        // Adding a sample contact for demonstration
-        Contact contact = new Contact("Jeffrey", "Samuel", "12 Main St", "Chennai", "TamilNadu", "129845", "9876543210", "je.sam@example.com");
-        addressBook.addContact(contact);
+        while (true) {
+            System.out.println("Enter first name:");
+            String firstName = scanner.nextLine();
+            System.out.println("Enter last name:");
+            String lastName = scanner.nextLine();
+            System.out.println("Enter address:");
+            String address = scanner.nextLine();
+            System.out.println("Enter city:");
+            String city = scanner.nextLine();
+            System.out.println("Enter state:");
+            String state = scanner.nextLine();
+            System.out.println("Enter zip:");
+            String zip = scanner.nextLine();
+            System.out.println("Enter phone number:");
+            String phoneNumber = scanner.nextLine();
+            System.out.println("Enter email:");
+            String email = scanner.nextLine();
 
-        // Delete contact
-        System.out.print("Enter First Name of the contact to delete: ");
-        String deleteFirstName = scanner.nextLine();
-        System.out.print("Enter Last Name of the contact to delete: ");
-        String deleteLastName = scanner.nextLine();
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            addressBook.addContact(contact);
 
-        addressBook.deleteContact(deleteFirstName, deleteLastName);
-        System.out.println("Contact deleted successfully!");
+            System.out.println("Do you want to add another contact? (yes/no)");
+            String response = scanner.nextLine();
+            if (!response.equalsIgnoreCase("yes")) {
+                break;
+            }
+        }
 
         addressBook.displayContacts();
     }
