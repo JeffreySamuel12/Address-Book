1fddc45776a22209df48663bf03eef00581696a2
Jeffrey Samuel <Jeffrey.Samuel@godigit.com>
2024-07-26 10:23:53 +0530

    Added Contact class and AddressBook class

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,6 +1,12 @@
 public class AddressBookMain {
         public static void main(String[] args) {
             System.out.println("Welcome to Address Book Program");
+
+            AddressBook addressBook = new AddressBook();
+            Contact contact = new Contact("Joseph", "Raj", "123 Main St", "San Andreas", "Gujarat", "12345", "123-456-7890", "joseph.raj@example.com");
+            addressBook.addContact(contact);
+
+            addressBook.displayContacts();
         }
-    }
+}
 
